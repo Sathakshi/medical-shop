41f34ae4f94d629ec128671dd82e173c070c4201
P.sathakshi sai <sathakshi.pokuri@gmail.com>
2024-07-08 13:50:05 +0530

    Initial commit

--- mongo.js
+++ mongo.js
@@ -6,6 +6,6 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
 })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch(err => console.log('MongoDB connection error:', err));
 
 module.exports = mongoose;
